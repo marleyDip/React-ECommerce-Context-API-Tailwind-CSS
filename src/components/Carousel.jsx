a205a87f8933c8eb9878d6f58a2cd25e5c3400bf
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-19 02:22:49 +0600

    useContext

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,14 @@
+import { useContext, useEffect } from "react";
+import { DataContext } from "../context/DataContext";
+
+const Carousel = () => {
+  const { fetchAllProducts } = useContext(DataContext);
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  return <div>Carousel</div>;
+};
+
+export default Carousel;
