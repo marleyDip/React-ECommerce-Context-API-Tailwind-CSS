4489bb69df590c79711265741f13f615f8cac126
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-18 14:53:33 +0600

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="underline">App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
+        <Route path="/products" element={<Products />}></Route>
+
+        <Route path="/about" element={<About />}></Route>
+
+        <Route path="/contact" element={<Contact />}></Route>
+
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
