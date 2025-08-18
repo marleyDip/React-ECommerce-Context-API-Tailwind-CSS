bc9a462c20a7cb1166c988d287662465349ee326
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-19 02:20:33 +0600

    Context API setup

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
+
+export const DataContext = createContext(null);
+
+//export const useDataContext = () => useContext(DataContext);
+
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState();
+
+  // fetching all products from API
+  const fetchAllProducts = async () => {
+    try {
+      const res = await axios.get(
+        "https://fakestoreapi.in/api/products?limit=150"
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+
+    /* const value = {data, setData, fetchAllProducts} */
+  };
+
+  return (
+    <DataContext.Provider value={{ data, setData, fetchAllProducts }}>
+      {children}
+    </DataContext.Provider>
+  );
+
+  /* return (
+    <DataContext.Provider value={value}>
+      {children}
+    </DataContext.Provider>
+  ); */
+};
