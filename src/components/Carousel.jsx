fb463551f951294798e42d302a6a94b3303b9cd6
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-19 02:38:55 +0600

    setup react slick carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,51 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { useContext, useEffect } from "react";
 import { DataContext } from "../context/DataContext";
+import Slider from "react-slick";
 
 const Carousel = () => {
-  const { fetchAllProducts } = useContext(DataContext);
+  const { data, fetchAllProducts } = useContext(DataContext);
+
+  console.log(data);
 
   useEffect(() => {
     fetchAllProducts();
   }, []);
 
-  return <div>Carousel</div>;
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default Carousel;
