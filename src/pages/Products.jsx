fbcf0e3e46f436ca46fc6d8d95fa37442de4b63c
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-20 18:43:07 +0600

    filtered data by search, category, brand, priceRange

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GetData } from "../context/DataContext";
 import FilterSection from "../components/FilterSection";
 
@@ -7,6 +7,43 @@ import ProductCard from "../components/ProductCard";
 
 const Products = () => {
   const { data, fetchAllProducts } = GetData();
+
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [priceRange, setPriceRange] = useState([0, 5000]);
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    //console.log(category);
+  };
+
+  const handleBrandChange = (e) => {
+    setBrand(e.target.value);
+  };
+
+  const handleMinChange = (e) => {
+    const newMin = Number(e.target.value);
+    if (newMin <= priceRange[1]) {
+      setPriceRange([newMin, priceRange[1]]);
+    }
+  };
+
+  const handleMaxChange = (e) => {
+    const newMax = Number(e.target.value);
+    if (newMax >= priceRange[0]) {
+      setPriceRange([priceRange[0], newMax]);
+    }
+  };
+
+  const filteredData = data?.filter(
+    (item) =>
+      item.title.toLowerCase().includes(search.toLowerCase()) &&
+      (category === "All" || item.category === category) &&
+      (brand === "All" || item.brand === brand) &&
+      item.price >= priceRange[0] &&
+      item.price <= priceRange[1]
+  );
 
   useEffect(() => {
     fetchAllProducts();
@@ -17,10 +54,23 @@ const Products = () => {
       <div className="max-w-6xl mx-auto px-4 mb-10">
         {data?.length > 0 ? (
           <div className="flex gap-8">
-            <FilterSection />
+            <FilterSection
+              search={search}
+              setSearch={setSearch}
+              category={category}
+              setCategory={setCategory}
+              handleCategoryChange={handleCategoryChange}
+              brand={brand}
+              handleBrandChange={handleBrandChange}
+              setBrand={setBrand}
+              priceRange={priceRange}
+              setPriceRange={setPriceRange}
+              handleMinChange={handleMinChange}
+              handleMaxChange={handleMaxChange}
+            />
 
             <div className="grid grid-cols-3 gap-7 mt-10">
-              {data?.map((product, index) => {
+              {filteredData?.map((product, index) => {
                 return <ProductCard key={index} product={product} />;
               })}
             </div>
