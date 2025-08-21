d0e59e691a951470fc3622ff241feeb962b9eda0
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-21 09:15:04 +0600

    Products all features complete

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { GetData } from "../context/DataContext";
+
 import FilterSection from "../components/FilterSection";
+import ProductCard from "../components/ProductCard";
+import Pagination from "../components/Pagination";
 
 import loading from "../assets/Loading4.webm";
-import ProductCard from "../components/ProductCard";
+import notfound from "../assets/No-Data.json";
+import Lottie from "lottie-react";
 
 const Products = () => {
   const { data, fetchAllProducts } = GetData();
@@ -13,13 +17,27 @@ const Products = () => {
   const [brand, setBrand] = useState("All");
   const [priceRange, setPriceRange] = useState([0, 5000]);
 
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchAllProducts();
+    window.scrollTo(0, 0);
+  }, []);
+
+  /* Why use window.scrollTo(0, 0) here?
+  
+  => When you navigate to this page (say a product listing), you want the user to start at the top of the page.
+  => Without it, if they were scrolled down on a previous page, they might land on this page already scrolled down, which feels wrong. */
+
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
     //console.log(category);
+    setPage(1);
   };
 
   const handleBrandChange = (e) => {
     setBrand(e.target.value);
+    setPage(1);
   };
 
   const handleMinChange = (e) => {
@@ -36,6 +54,10 @@ const Products = () => {
     }
   };
 
+  const pageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
+
   const filteredData = data?.filter(
     (item) =>
       item.title.toLowerCase().includes(search.toLowerCase()) &&
@@ -45,36 +67,69 @@ const Products = () => {
       item.price <= priceRange[1]
   );
 
-  useEffect(() => {
-    fetchAllProducts();
-  }, []);
+  const dynamicPage = Math.ceil(filteredData?.length / 6);
+
+  /* 
+      => This code is using JavaScript’s Array.prototype.filter() method, and the function you’ve written inside is a predicate (callback) function.
+      
+      => data?.filter(...) → calls the .filter() method on an array (if data isn’t null/undefined).
+      => Inside, (item) => { ... } → this is an arrow function (anonymous function).
+      => .filter() expects a predicate function → a function that returns true or false for each element.
+      => If it returns true, the element is kept in the new array.
+      => If it returns false, the element is filtered out.
+
+      => This is an arrow function used as a predicate function inside Array.filter().
+  */
 
   return (
     <div>
       <div className="max-w-6xl mx-auto px-4 mb-10">
         {data?.length > 0 ? (
-          <div className="flex gap-8">
-            <FilterSection
-              search={search}
-              setSearch={setSearch}
-              category={category}
-              setCategory={setCategory}
-              handleCategoryChange={handleCategoryChange}
-              brand={brand}
-              handleBrandChange={handleBrandChange}
-              setBrand={setBrand}
-              priceRange={priceRange}
-              setPriceRange={setPriceRange}
-              handleMinChange={handleMinChange}
-              handleMaxChange={handleMaxChange}
-            />
+          <>
+            <div className="flex gap-8">
+              <FilterSection
+                search={search}
+                setSearch={setSearch}
+                category={category}
+                setCategory={setCategory}
+                handleCategoryChange={handleCategoryChange}
+                brand={brand}
+                handleBrandChange={handleBrandChange}
+                setBrand={setBrand}
+                priceRange={priceRange}
+                setPriceRange={setPriceRange}
+                handleMinChange={handleMinChange}
+                handleMaxChange={handleMaxChange}
+              />
 
-            <div className="grid grid-cols-3 gap-7 mt-10">
-              {filteredData?.map((product, index) => {
-                return <ProductCard key={index} product={product} />;
-              })}
+              {filteredData?.length > 0 ? (
+                <div className="flex flex-col items-center justify-center">
+                  <div className="grid grid-cols-3 gap-7 mt-10">
+                    {filteredData
+                      ?.slice(page * 6 - 6, page * 6)
+                      .map((product, index) => {
+                        return <ProductCard key={index} product={product} />;
+                      })}
+                  </div>
+
+                  <Pagination
+                    page={page}
+                    pageHandler={pageHandler}
+                    dynamicPage={dynamicPage}
+                  />
+                </div>
+              ) : (
+                <div className="flex items-center justify-center md:h-[600px] md:w-[900px] mt-10">
+                  <Lottie
+                    animationData={notfound}
+                    style={{ width: 500, height: 500 }}
+                  />
+
+                  {/* <Lottie animationData={notfound} classID="w-[500px]" /> */}
+                </div>
+              )}
             </div>
-          </div>
+          </>
         ) : (
           <div className="flex items-center justify-center h-[400px]">
             <video muted autoPlay loop>
