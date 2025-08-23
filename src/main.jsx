d0c744a81af560c8601b4ef43e62b90457fc71e3
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-23 23:52:23 +0600

    scroll to top implement

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { DataProvider } from "./context/DataContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 
 import { ToastContainer } from "react-toastify";
+import ScrollToTop from "react-scroll-to-top";
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -25,6 +26,18 @@ createRoot(document.getElementById("root")).render(
       <CartProvider>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
+
+          <ScrollToTop
+            color="white"
+            smooth
+            style={{
+              backgroundColor: "#fa2d37",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          />
+
           <ToastContainer
             position="bottom-right"
             autoClose={5000}
